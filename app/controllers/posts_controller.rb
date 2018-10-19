1e5717dd1cc302d8cc6f78798a70eb90ea7c7fbe
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-10-19 00:59:01 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,14 @@ class PostsController < ApplicationController
     if !params[:author].blank?
       @posts = Post.by_author(params[:author])
     elsif !params[:date].blank?
-      
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      @posts = Post.all
     end
-    @posts = Post.all
-    
   end
 
   def show
